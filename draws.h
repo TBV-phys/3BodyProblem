06394ca19036f0b634fc10e73c848dc4bfb2594b
TBV <teodor@iMac-de-Teodor.local>
2026-04-08 13:13:02 +0200

    Project uploaded

---
+++ draws.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void drawCircle(float cx, float cy, float r, int segments, const std::array<float,3>& colorRGB);
+
+void drawTrail(const std::vector<double>& x1, const std::vector<double>& y1, const int& cont, const int& ptsColaMax, const std::array<float,3>& colorRGB);
---
+++ verlet.h
@@ -0,0 +1,9 @@
+//
+//  verlet.h
+//  3BodyProblem
+//
+//  Created by Teodor Borislavov Vassilev on 12/2/26.
+//
+#pragma once
+
+void verlet(double& x_prev, double& y_prev, double& x, double& y, double& vx, double& vy, double& ax, double& ay, float dt);
